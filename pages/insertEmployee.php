32f55e0ad6f48b13640b2bdd91642b5bd739e432
TaehyungAlexKim <63694948+TaehyungAlexKim@users.noreply.github.com>
2021-02-02 23:32:46 -0500

    fileUpload

--- pages/insertEmployee.php
+++ pages/insertEmployee.php
@@ -41,7 +41,6 @@
 
     // Auto Mock Generator and symfony
     require_once '../vendor/autoload.php';
-
     ?>
 </head>
 
@@ -68,35 +67,34 @@
             </script>
         <?php
         }
-        // Click - date save 
-        else if (isset($field_data['save'])) {
-            //dump($_POST);
-
-            // Duplicate Check and than, insert date
-            duplicateCheck();
-
-            // show date from database
-            showDataFromDatabase($db_conn);
-        }
         // when file is uploaded...
         else if (file_exists($_FILES['file']['tmp_name']) || is_uploaded_file($_FILES['file']['tmp_name'])) {
-            $allowed_ext = array('csv');
+            // Check the Meta Data
+            if ($_FILES['file']['type'] == 'text/csv' && $_FILES['file']['error'] == 0) {
 
-            if (!in_array(array_pop(explode('.', $_FILES['file']['name'])), $allowed_ext)) {
-                $msg = "You can only upload files with the cvs extension.";
-                echo "<div class=\"alert alert-danger\" role=\"alert\">" . $msg . "</div>";
-                showUploadForm();
-            } else {
-                $fh = fopen($_FILES['file']['tmp_name'], 'r+');
+                $destination_file = '../file/employeeList_' . time() . '.csv';
+                move_uploaded_file($_FILES['file']['tmp_name'], $destination_file);
 
-                while (($row = fgetcsv($fh, 8192)) !== FALSE) {
-                    $lines[] = $row;
-                }
-                // show the data from file
-                makeTable('csv', $lines);
+                if (($handle = fopen($destination_file, "r")) !== FALSE) {
                 
-            } 
-        }else if(isset($field_data['showdata'])){
+                    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+                        $num = count($data);
+                        $lines[] = $data;
+                    }
+                    fclose($handle);
+                }
+                // Check Duplicate and Insert Data
+                duplicateCheck($lines);
+
+                // show date from database
+                showDataFromDatabase($db_conn);
+
+            } else {
+                $msg = 'It is not a CSV file. Please upload again.';
+                makeHeader('fileuploadfail');
+                showUploadForm();
+            }
+        } else if (isset($field_data['showdata'])) {
             showDataFromDatabase($db_conn);
         }
         // Main page
@@ -131,20 +129,19 @@
             }
         }
 
-        function duplicateCheck()
+        function duplicateCheck($arg)
         {
             global $msg;
             global $db_conn;
 
-            $emps = unserialize(urldecode($_POST['array']));
             $newArr = array();
-            foreach ($emps as $val) {
+            foreach ($arg as $val) {
                 $newArr[$val[0] . $val[1] . $val[3] . $val[4]] = $val;
             }
             $unUnique = array_values($newArr);
-            if (count($unUnique) != count($emps)) {
-                $msg = 'In the uploaded CSV file, there is ' . number_format(count($emps) - count($unUnique)) . ' data with the same name, gender, and birthday. ' . count($unUnique) . ' data is inserted.';
-            } elseif (count($unUnique) == count($emps)) {
+            if (count($unUnique) != count($arg)) {
+                $msg = 'In the uploaded CSV file, there is ' . number_format(count($arg) - count($unUnique)) . ' data with the same name, gender, and birthday. ' . count($unUnique) . ' data is inserted.';
+            } elseif (count($unUnique) == count($arg)) {
                 $msg =  count($unUnique) . ' data is inserted.';
             }
             insertEmp($db_conn, $unUnique);
@@ -152,6 +149,13 @@
 
         function insertEmp($dbc, $arr)
         {
+            try {
+                $stmt = $dbc->prepare("TRUNCATE `lamp`.`employees`");
+                $stmt->execute();
+            } catch (Exception $e) {
+                $dbc->rollback();
+                echo $e->getMessage();
+            }
             $stmt = $dbc->prepare("INSERT INTO employees (last_name, first_name, middle_name, date_of_birth, gender, date_hired, hired_salary_level, job_type, last_updated_user_id) values(?, ?, ?, ?, ?, ?, ?, ?, 'CSVinsert')");
 
             try {
@@ -171,12 +175,10 @@
         function makeTable($type, $arg)
         {
             global $table;
-            if ($type == 'csv') {
-                $number = 'temp';
-            }
+           
             $table = "<table class='table table-striped table-hover'><thead>
         <tr>
-          <th scope='col'>" . $number . "#</th>
+          <th scope='col'>#</th>
           <th scope='col'>Surname</th>
           <th scope='col'>GivenName</th>
           <th scope='col'>MiddleName</th>
@@ -209,30 +211,15 @@
             global $header;
             global $lines;
             global $msg;
-            if ($type == 'csv') {
-                $header = "<div class='alert alert-danger' role='alert'>
-                The data below has not yet been saved! Click the 'Save'button after data check.</div>
-                <form method='POST'>
-                <div class='row g-2 d-flex justify-content-end'>
-                <div class='col-auto'>
-                    <button type='submit' name='again' class='btn btn-warning' value='again'>Try again</button>
-                    </div>    
-                <div class='col-auto'>
-                    <input type='hidden' name='array' value='" . urlencode(serialize($lines)) . "'>
-                    <button type='submit' name='save' class='btn btn-success' value='save'>Save</button>
-                    </div>
-                </div>
-                </form>
-                ";
-                echo $header;
-            } else if ($type == 'db') {
-                if(strlen($msg)>0){
+            if ($type == 'db') {
+                if (strlen($msg) > 0) {
                     $header = "<div class=\"alert alert-success\" role=\"alert\">" . $msg . "</div>";
                     echo $header;
                 }
-                
+            } else if ($type == 'fileuploadfail') {
+                $header = "<div class=\"alert alert-danger\" role=\"alert\">" . $msg . "</div>";
+                echo $header;
             }
-       
         }
 
         function showUploadForm()
@@ -241,7 +228,7 @@
 
             <form method="POST" enctype="multipart/form-data">
                 <div class="mb-3">
-                    <label for="formFile" class="form-label">Please select the CSV file... <br> If there is data in the CSV file with the same first, last name, gender and birthday, the data will merged.</label>
+                    <label for="formFile" class="form-label">Please select the CSV file... <br>Click "Choose File" to select a CSV file and click the Upload button.<br>Please refer to the "CSV Generator" menu for the format of the CSV file.<br>If there is data in the CSV file with the same first, last name, gender and birthday, the data will merged.</label>
                     <div class="row g-2">
                         <div class="col-auto">
                             <input class="form-control" type="file" id="formFile" name="file">
