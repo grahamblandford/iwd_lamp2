f79055675e796ad25e9e8cb999acb1e46434c146
TaehyungAlexKim <63694948+TaehyungAlexKim@users.noreply.github.com>
2021-02-26 00:18:27 -0500

    Update index.php

--- index.php
+++ index.php
@@ -47,7 +47,7 @@
     <main class="container">
 
         <div class="starter-template text-center py-5 px-3">
-            <h1>Bootstrap starter template </h1>
+            <h1>Update test </h1>
             <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text
                 and a mostly barebones HTML document.</p>
         </div>
